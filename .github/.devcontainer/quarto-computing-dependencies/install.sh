b8c8ba721650b0e5ee408acd2efd46277e2d934c
Julia Silge <julia.silge@gmail.com>
2025-05-14 12:14:17 -0600

    Try out devcontainer

--- .github/.devcontainer/quarto-computing-dependencies/install.sh
+++ .github/.devcontainer/quarto-computing-dependencies/install.sh
@@ -6,9 +6,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 USERNAME=${USERNAME:-${_REMOTE_USER:-"automatic"}}
 
-R_DEPS=${RDEPS:-"rmarkdown"}
 PYTHON_DEPS=${PYTHONDEPS:-"jupyter,papermill"}
-JULIA_DEPS=${JULIADEPS:-"IJulia"}
 
 if [ "$(id -u)" -ne 0 ]; then
   echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
@@ -32,24 +30,11 @@ elif [ "${USERNAME}" = "none" ] || ! id -u "${USERNAME}" >/dev/null 2>&1; then
   USERNAME=root
 fi
 
-quarto_r_deps() {
-  local deps=$1
-  deps=$(echo "${deps}" | sed 's/,/","/g')
-  su "${USERNAME}" -c "Rscript -e 'pak::pkg_install(c(\"${deps}\"))'"
-}
-
 quarto_python_deps() {
   local deps=$1
   deps=$(echo "${deps}" | sed 's/,/ /g')
   python3 -m pip install ${deps}
+  python3 -m pip install -r requirements.txt
 }
 
-quarto_julia_deps() {
-  local deps=$1
-  deps=$(echo "${deps}" | sed 's/,/","/g')
-  su "${USERNAME}" -c "~/.juliaup/bin/julia -e 'using Pkg; Pkg.add.([\"${deps}\"])'"
-}
-
-quarto_r_deps ${R_DEPS}
 quarto_python_deps ${PYTHON_DEPS}
-quarto_julia_deps ${JULIA_DEPS}
